1a37740820fd250879360665e03d351c2196e4bb
Sweekrithi  S  Shetty <shettysweekrithi03@gmail.com>
2021-07-28 09:45:00 -0400

    Add files via upload

---
+++ hello world.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 26 18:02:35 2021
+
+@author: SWEEKRITHI SHETTY
+"""
+
+print('Hello world!')
+print(7/3)
+
+print(2 **8)
+print('how are \n you today')
+print('how\'s it going ')
+print("how's it going")
+print('one',end = ' ')
+print('two',end = ' ')
+print('three',end = ' ')
+print('four')
+print("One,'two',\"three\",four,\\five\\once,'I' caught a fish '//alive\\\\'")
+
+print('One,\'two\',\"three\",four,\\five\\\n\tonce,\'I\' caught a\
+ fish\n\t\t\'//alive\\\\\'')
---
+++ variables.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 26 18:31:20 2021
+
+@author: SWEEKRITHI SHETTY
+"""
+
+x = 5
+y = 12
+z = x +y
+#strings
+#user_input =    input('How many apples do you have? \n >>>')
+#user_input1 =    int(input('How many apples do you have? \n >>>'))
+#help()
+# Ask the user for the radius of a circle and then print the area
+# radius = float(input('Please enter the radius: '))
+# pi  = 3.14159
+# area = pi * radius
+# print('You enter the radius: ',radius,'The area of the circle is',area)
+
+#Convert fahrenheit to celsius
+# Far  = float(input('Enter the temperature in Fahrenheit: '))
+# cel = (Far - 32) * 5 /9
+# print(Far,'Fahrenheit in Celsius is',cel)
+
+#Obtain the sum of two integers3
+# num_1 = int(input('Please enter the first number'))
+# num_2 = int(input('Please enter the second number'))
+# print('The sum of ' + str(num_1) + ' and ' + str(num_2) + ' is ' + str(num_1 + num_2))
+
+#Obtain the product of two integer
+s
+
+#num_1 = int(input('Please enter the first number:'))
+#num_2 = int(input('Please enter the second number:'))
+#product = num_1 * num_2
+#print('The product of',num_1,'and',num_2,'is',product)
+
+# Bob, Ann, Jane and Ashwin want to order pizza - they know they will each eat
+# 4 slices of pizza. The local pizza shop sells pizzas of only 6 slices
+# What is the minimum number of pizzas needed - Use floor division4
+total_slices = 4 * 4
+number_of_pizza = (total_slices + 5)// 6 
+slices_left = number_of_pizzas * 6 - total_slices 
+print('Number of pizzas required is',number_of_pizzas,'there will be',slices_left,\
+'remaining slices.')
+    
+    
+
+
+
